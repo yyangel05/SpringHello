b81d8896a014af1e91b1c300a27b7f7b0ec1b532
KH_I\user1 <hyunhwa91@naver.com>
2018-06-25 13:37:11 +0900

    계정바꾸기 테스트

---
+++ src/controller/NewArticleController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import service.ArticleService;
+import service.NewArticleCommand;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/article/newArticle.do")
+public class NewArticleController {
+	@Autowired
+	private ArticleService articleService;
+	
+	@RequestMapping(method= RequestMethod.GET)
+	public String form() {
+		return "article/newArticleForm";
+	}
+	
+	@RequestMapping(method= RequestMethod.POST)
+	public String submit(@ModelAttribute("command") NewArticleCommand command) {
+		articleService.writeArticle(command);
+		return "article/newArticleSubmitted";
+	}
+	
+	public void setArticleService(ArticleService articleService) {
+		this.articleService = articleService;
+	}
+
+}
