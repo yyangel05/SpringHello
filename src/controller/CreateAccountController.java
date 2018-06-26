8f9097bad4e1fd95122e1e2c9683b2a4bb116122
user1 <yyangel@gmail.com>
2018-06-26 15:05:50 +0900

    180626 예제007코드만 작성하고 테스트 함.

--- src/controller/CreateAccountController.java
+++ src/controller/CreateAccountController.java
@@ -1,5 +1,53 @@
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import model.Address;
+import model.MemberInfo;
+import validator.MemberInfoValidator;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/account/create.do")
 public class CreateAccountController {
+	
+	@ModelAttribute("command")
+	public MemberInfo formBacking(HttpServletRequest request) {
+		if(request.getMethod().equalsIgnoreCase("GET")) {
+			MemberInfo mi = new MemberInfo();
+			Address address = new Address();
+			address.setZipcode(autoDetectZipcode(request.getRemoteAddr()));
+			mi.setAddress(address);
+			return mi;
+		} 
+		else {
+			return new MemberInfo();
+		}
+	}
+	
+	private String autoDetectZipcode(String remoteAddr) {
+		return "000000";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET) 
+	public String form() {
+		return "account/creationForm";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST) 
+	public String submit(@ModelAttribute("command") MemberInfo memberInfo, BindingResult result) {
+		new MemberInfoValidator().validate(memberInfo, result);
+		if(result.hasErrors()) {
+			return "account/creationForm";
+		}
+		
+		return "account/created";
+	}
+	
 
 }
